7ff681e597bf5a01450d619365a9e8cae2107c5e
Sami Hatoum <shatoum101@gmail.com>
2025-07-27 15:54:13 +0100

    api fix

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { Tabs } from './components/Tabs';
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  const backendIP = "https://fwm-backend.vercel.app";
+  const backendIP = "https://fwm-backend.vercel.app/api";
   // Enter IP here
   const [ip, setIP] = useState(YourIP);
 
