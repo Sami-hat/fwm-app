1b9fe1c05455fc7915e18d8f864805c31c4420d4
Sami Hatoum <shatoum101@gmail.com>
2025-08-02 12:59:00 +0100

    backend rm

--- backend/sql/queries.sql
+++
@@ -1,9 +0,0 @@
--- INSERT INTO inventorytable (name, quantity, userid)
--- VALUES ('caster sugar', '1000g', 1);
-
--- INSERT INTO profiletable (email, password)
--- VALUES ('abc@gmail.com', 'abc');
-
-SELECT * FROM profiletable;
-
--- SELECT * FROM inventorytable;
